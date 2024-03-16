f2040512bd659d8d921094d3c9e4f1202aaa6ba1
ppppppsmash <pppppp.work@gmail.com>
2024-03-16 11:42:22 +0900

    test: add the schema field

--- sanity/schemas/top.ts
+++ sanity/schemas/top.ts
@@ -1,13 +1,25 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'event',
-  title: 'Event',
+  name: 'Top',
+  title: 'トップ',
   type: 'document',
   fields: [
     defineField({
-      name: 'name',
+      title: 'Title',
+      name: 'title', // FiledName
       type: 'string',
     }),
+    defineField({
+      title: 'Description',
+      name: 'description',
+      type: 'string',
+    }),
+    defineField({
+      title: 'Body',
+      name: 'body',
+      type: 'array',
+      of: [{ type: 'block' }]
+    })
   ],
 })
