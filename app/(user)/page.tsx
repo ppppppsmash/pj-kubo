2587ab2b9e33f859408dca3d348aef079c4119d6
ppppppsmash <pppppp.work@gmail.com>
2024-03-16 13:11:27 +0900

    test: type for sanity

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,31 +1,64 @@
 import { groq } from 'next-sanity'
 import { client } from '@/sanity/lib/client'
 
-const queryPrimary = groq`
+interface TopDoc {
+  children: {
+    text: string
+  }[]
+}
+
+interface Top {
+  title: string
+  description: string
+  body: TopDoc[]
+}
+
+
+
+const query = groq`
   *[_type == 'top'] {
     title,
     description,
     body
-  }[0]
+  }
 `
 
 export default async function Home() {
-  const data = await client.fetch(queryPrimary)
+  const data: Top[] = await client.fetch(query)
+  console.log(data)
 
   return (
     <main>
       <section className="my-10 border">
         <h1>secion 1</h1>
-        <h3>{data?.title}</h3>
-        <p>{data?.description}</p>
+        <h3>{data[0]?.title}</h3>
+        <p>{data[0]?.description}</p>
 
-        <div>
-          {data?.body}
-        </div>
+        <div className="mt-6">
+          {data[0]?.body.map((block, index) => (
+            <div key={index}>
+              {block.children.map((area, childIndex) => (
+                <p key={childIndex}>{area.text}</p>
+              ))}
+            </div>
+          ))}
+        </div>        
       </section>
 
       <section className="my-10 border">
+        <h1>secion 2</h1>
+        <h3>{data[1]?.title}</h3>
+        <p>{data[1]?.description}</p>
 
+        <div className="mt-6">
+          {data[1]?.body.map((block, index) => (
+            <div key={index}>
+              {block.children.map((child, childIndex) => (
+                <p key={childIndex}>{child.text}</p>
+              ))}
+            </div>
+          ))}
+        </div>
       </section>
       
     </main>
