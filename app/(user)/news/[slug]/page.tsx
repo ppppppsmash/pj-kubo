3a2f373ca2385247b48dbc53dcd0831d0b8654ca
ppppppsmash <pppppp.work@gmail.com>
2024-03-27 22:59:27 +0900

    style: news page style

--- app/(user)/news/[slug]/page.tsx
+++ app/(user)/news/[slug]/page.tsx
@@ -24,28 +24,38 @@ export default function Page({ params } : { params: { slug: string }}) {
       </section>
 
       <section>
-        <div className="max-w-[900px] w-full mx-auto mt-5 mb-16">
-          <img className="rounded-lg" src="/images/main-visual.jpg" />
+        <div className="max-w-[900px] w-full mx-auto sm:mt-5 mb-7 sm:mb-16">
+          <img className="sm:rounded-lg" src="/images/main-visual.jpg" />
         </div>
 
-        <div className="max-w-[800px] w-full mx-auto mt-5 mb-16">
+        <div className="max-w-[800px] w-full mx-auto mt-2 sm:mt-5 mb-16">
           <p className="mb-5 px-6">哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍<br />
           哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍<br />
           哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍<br />
           哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍
           </p>
 
-          <h2 className="sm:w-[calc(100%_-_50px)] sm:p-6 sm:mt-10 sm:mb-5 bg-gray font-bold leading-[1.4] sm:rounded-[25px] text-[24px]">副標題副標題副標題副標題副標題副標題</h2>
+          <h2
+            className="w-[calc(100%_-_25px)] sm:w-[calc(100%_-_50px)] py-3 px-6 sm:p-5 sm:mt-10 sm:mb-5 bg-gray font-bold leading-[1.4]
+              rounded-tr-[25px] rounded-br-[25px] sm:rounded-[25px] text-[24px]"
+          >
+            副標題副標題副標題
+          </h2>
 
-          <p className="mb-5 px-6">哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍<br />
+          <p className="my-5 px-6">哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍<br />
           哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍<br />
           哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍<br />
           哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍
           </p>
 
-          <h2 className="sm:w-[calc(100%_-_50px)] sm:p-6 sm:mt-10 sm:mb-5 bg-gray font-bold leading-[1.4] sm:rounded-[25px] text-[24px]">副標題副標題副標題副標題副標題副標題</h2>
+          <h2
+            className="w-[calc(100%_-_25px)] sm:w-[calc(100%_-_50px)] py-3 px-6 sm:p-5 sm:mt-10 sm:mb-5 bg-gray font-bold leading-[1.4]
+              rounded-tr-[25px] rounded-br-[25px] sm:rounded-[25px] text-[24px]"
+          >
+            副標題副標題副標題
+          </h2>
 
-          <p className="mb-5 px-6">哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍<br />
+          <p className="my-5 px-6">哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍<br />
           哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍<br />
           哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍<br />
           哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍哈嘍
